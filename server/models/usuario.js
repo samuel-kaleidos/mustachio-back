9a0febe20994b4d61bcb99ff5d70e7551a16375d
Samuel Gomez <samuel.gomez@kaleidos.net>
2021-02-05 08:56:41 +0100

    #00001 Add Login and register

---
+++ server/models/usuario.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+ rolesValidos = {
+  values: ["ADMIN", "USER"],
+  message: '{VALUE} no es un role válido'
+}
+const Schema = mongoose.Schema;
+
+const usuarioSchema = new Schema({
+  nombre: {
+      type: String,
+      required: [true, 'El nombre es necesario'],
+  },
+  email: {
+      type: String,
+      unique: true,
+      required: [true, "El correo es necesario"],
+  },
+  password: {
+      type: String,
+      required: [true, "Le contraseña es obligatoria"],
+  },
+  role: {
+      type: String,
+      default: 'USER',
+      required: [true],
+      enum: rolesValidos,
+  },
+});
+
+// elimina la key password del objeto que retorna al momento de crear un usuario
+usuarioSchema.methods.toJSON = function() {
+  let user = this;
+  let userObject = user.toObject();
+  delete userObject.password;
+  return userObject;
+}
+
+usuarioSchema.plugin(uniqueValidator, {
+  message: '{PATH} debe de ser único'
+})
+module.exports = mongoose.model('Usuario', usuarioSchema)
---
+++ server/routes/register.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const Usuario = require('./../models/usuario');
+const app = express();
+app.post('/register', function (req, res) {
+  const body = req.body;
+  const { nombre, email, password, role } = body;
+  const usuario = new Usuario({
+    nombre,
+    email,
+    password: bcrypt.hashSync(password, 10),
+    role
+  });
+  usuario.save((err, usuarioDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+    res.json({
+      ok: true,
+      usuario: usuarioDB
+    });
+  })
+});
+module.exports = app;
